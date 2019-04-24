3ff6d9aff0e70c5bf00aa70bcdbc3af7b3deee3e
Matt <bpmjg3@gmail.com>
2019-04-23 20:40:03 -0700

    updating main and testing factory

--- src/main/java/assignment5/Main.java
+++ src/main/java/assignment5/Main.java
@@ -1,20 +1,23 @@
 package main.java.assignment5;
 
 import Factory.HondaMaker;
+import GUI.CarGUI;
 
 public class Main {
 
     public static void main(String[] args) {
         //create
-        HondaMaker Accord = new HondaMaker("Perry Honda", "Accord");
-        Accord.setPaint("Black");
-        Accord.setCarType("Sedan");
-        Accord.setEngine(6);
-        Accord.setChassis("Steel");
-        Accord.setTransmission("Manual");
-        Accord.setTire(19);
+        // HondaMaker Accord = new HondaMaker("Perry Honda", "Accord");
+        // Accord.setPaint("Black");
+        // Accord.setCarType("Sedan");
+        // Accord.setEngine(6);
+        // Accord.setChassis("Steel");
+        // Accord.setTransmission("Manual");
+        // Accord.setTire(19);
 
-        Accord.calcPrice();
+        // Accord.calcPrice();
+
+        CarGUI ui = new CarGUI();
     }
 
 }
